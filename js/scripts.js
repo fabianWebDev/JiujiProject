80fcf34f21ef02b1128e0b98845d46e6565e3156
fabcava22 <fabian.campos@co4c.com>
2020-02-29 17:50:59 -0600

    firts add of files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let main_card = document.getElementsByClassName("main_card");
+let tech_card_info = document.getElementsByClassName("tech_card");
+
+
+let tech_img = [];
+
+tech_card_info = `
+<h2></h2>
+<img></img>
+<p></p>
+`
